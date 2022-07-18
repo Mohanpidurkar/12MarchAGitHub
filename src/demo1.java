d6bf9bfce309b526a2e0b940c7fe1020ff5412ee
mohan <mohan@LAPTOP-B3GG8RMS>
2022-07-18 14:01:17 +0530

    [Mohan]:1st changes in project

---
+++ src/demo1.java
@@ -0,0 +1,14 @@
+
+public class demo1 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		
+	}
+
+}
